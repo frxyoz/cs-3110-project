ab68c163df7825c7a008a9b8c0aa5672f9c85c10
Talus <leongtalus@gmail.com>
2026-04-11 10:52:52 -0400

    Server is... kinda there?

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,19 @@
-let () = print_endline "Hello, World!"
+let client_handler client_socket_address (client_in, client_out) =
+  let addr_str =
+    match client_socket_address with
+    | Unix.ADDR_INET (inet_addr, port) ->
+        Printf.sprintf "%s:%d" (Unix.string_of_inet_addr inet_addr) port
+    | _ -> "unknown"
+  in
+  let%lwt () = Lwt_io.printlf "I got a connection from %s." addr_str in
+  let%lwt () =
+    Lwt_io.fprintf client_out "You are connected.\n"
+  in
+
+  (* IMPORTANT: flush so client actually receives it *)
+  let%lwt () = Lwt_io.flush client_out in
+
+  Lwt.return ()
 
 let run_server () =
   let server () =
